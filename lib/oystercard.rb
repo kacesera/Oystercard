c31364a1ef43fe243f845781a1aca4c15d49405e
Charlie Galbraith <galbraith.charlie@gmail.com>
2020-10-06 16:21:29 +0100

    Modify to #touch_in method to record entry station. Refactor #in_journey to infer status from @entry_station. Refactor tests to account for changes.

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,12 @@
 class Oystercard
-  attr_reader :balance, :status
+  attr_reader :balance, :status, :entry_station
   DEFAULT_MAX = 90
   MIN_FARE = 1
 
   def initialize
     @balance = 0
     @travelling = nil
+    @entry_station = nil
   end
 
   def top_up(cash)
@@ -13,18 +14,18 @@ class Oystercard
     add_money(cash)
   end
 
-  def touch_in
+  def touch_in(entry_station)
     raise "INSUFFICIENT FUNDS" if insufficient_funds?
-    @travelling = true
+    @entry_station = entry_station
   end
 
   def in_journey?
-    @travelling
+    !!@entry_station
   end
 
   def touch_out
     deduct_fare
-    @travelling = false
+    @entry_station = nil
     puts "Have a good day!"
   end
 
@@ -45,7 +46,7 @@ class Oystercard
 
   def over_max_balance?(cash)
     if (cash + balance) > DEFAULT_MAX
-      raise "Max balance of #{DEFAULT_MAX} is exceeded" 
+      raise "Max balance of #{DEFAULT_MAX} is exceeded"
     end
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,6 +2,7 @@ require 'oystercard'
 
 describe Oystercard do
   subject(:card) { Oystercard.new }
+  let(:mock_station) { double("fake entry station")}
 
   it 'initializes with a default balance of zero' do
     expect(card.balance).to be_zero
@@ -21,33 +22,47 @@ describe Oystercard do
   describe '#touch_in' do
     it 'changes the @status of the Oystercard to true' do
       card.top_up(5)
-      card.touch_in
+      card.touch_in(mock_station)
       expect(card.in_journey?).to be true
     end
 
     it 'throws an error if user tries to #touch_in with insufficient funds' do
-      expect{ card.touch_in }.to raise_error "INSUFFICIENT FUNDS"
+      expect{ card.touch_in(mock_station) }.to raise_error "INSUFFICIENT FUNDS"
     end
+
+    it "remembers the entry station where the user touched in" do
+      card.top_up(10)
+      card.touch_in(mock_station)
+      expect(card.entry_station).to eq(mock_station)
+    end
+
   end
 
   describe '#touch_out' do
     it 'changes the @status of the Oystercard to true' do
       card.top_up(5)
-      card.touch_in
+      card.touch_in(mock_station)
       card.touch_out
       expect(card.in_journey?).to be false
     end
 
     it 'says "Have a good day!"' do
       card.top_up(5)
-      card.touch_in
+      card.touch_in(mock_station)
       expect { card.touch_out }.to output("Have a good day!\n").to_stdout
     end
 
     it 'deducts minimum fare from balance' do
       card.top_up(5)
-      card.touch_in
+      card.touch_in(mock_station)
       expect { card.touch_out }.to change {card.balance}.by -1
+    end
+
+    it 'forgets entry station after touching out' do
+      card.top_up(5)
+      card.touch_in(mock_station)
+      card.touch_out
+      expect(card.entry_station).to eq(nil)
     end
 
   end
